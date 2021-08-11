f739f8b9f34e6eaad50ee7eed9b86add910280de
juanmanuel0 <86835421+juanmanuel0@users.noreply.github.com>
2021-08-11 10:45:08 -0300

    Add files via upload

--- tres.js
+++ tres.js
@@ -1,3 +1,89 @@
+/*Este fin de semana se festeja el día del niño y se deben gestionar las ventas de una jugueteria
+Obviamente se registran las ventas producidas ese día. De cada venta se registra:
+-tipo de juguete ("muñeca", “rompecabezas”, “pelota”)
+-origen ("nacional", "importado")
+-precio ( entre 1000 y 5000 pesos)
+Informar:
+a- El tipo de juguete mas vendido
+b- El promedio de precio de los juguetes importados
+c- La recaudacion total
+d- Cual es el precio del rompecabezas nacional mas barato
+e- Cuanto se percibio de iva en total (es el 21% de las ventas) */
 function mostrar() {
-  alert("Ejercicio 3");
-}
+  let tipo;
+  let origen;
+  let precio;
+  //punto A
+  let tipoMasVendido;
+  let contadorM = 0;
+  let contadorR = 0;
+  let contadorP = 0;
+  // punto B
+  let acumImportados = 0;
+  let contadorPrecioIm = 0;
+  let promImportados = 0;
+  // punto C
+  let recaudacionTotal = 0;
+  // punto D
+  let flag = 1;
+  let rompecabezasBarato = 0;
+  // punto E
+  let iva;
+  //////////// 
+  let respuesta;
+  do {
+    tipo = prompt("Ingrese un jugete (muñeca/rompecabezas/pelota)");
+    while (tipo != "rompecabezas" && tipo != "muñeca" && tipo != "pelota") {
+      tipo = prompt("Error. Ingrese un jugete (muñeca/rompecabezas/pelota)");
+    }
+    origen = prompt("ingresen el producto (nacional/importado)");
+		while (origen != "nacional" && origen != "importado") {
+			origen = prompt("Error. ingresen el producto (nacional/importado)");
+		}
+    precio = parseInt(prompt("ingrese un precio entre (1mil y 5mil)"));
+		while (!(precio >= 1000 && precio <= 5000)) {
+			precio = parseInt(prompt("Error. ingrese un numero entre (1mil y 5mil))"));
+		}
+    if (tipo == "muñeca") {
+      contadorM++;
+    }else if (tipo == "rompecabezas") {
+      contadorR++;
+    }else{
+      contadorP++;
+    }
+    if (origen == "importado") {
+      acumImportados++;
+      contadorPrecioIm += precio
+    }
+    if (origen == "nacional" && tipo == "rompecabezas") {
+      if (flag || precio < rompecabezasBarato ) {
+        rompecabezasBarato = precio;
+      }
+    }
+    recaudacionTotal += precio;
+
+    respuesta = prompt("quiere ingresar otra venta (si/no)")
+  } while (respuesta == "si");
+  if(contadorR > contadorM && contadorR > contadorP){
+    tipoMasVendido = "rompecabezas";
+  }else if (contadorM >= contadorR && contadorM > contadorP){
+   tipoMasVendido = "muñeca";
+  }else {
+   tipoMasVendido = "pelota";}
+
+   if (acumImportados != 0) {
+     promImportados = contadorPrecioIm / acumImportados;
+   }
+   iva = recaudacionTotal * 21 / 100;
+
+   document.write("a- el juge mas vendido es " + tipoMasVendido + "<br>");
+   document.write("b- el promedio de los jugete importados es " + promImportados + "<br>");
+   document.write("c- la recaudacion total fue " + recaudacionTotal + "<br>");
+   document.write("d- el rompecabezas nacional mas barato es "+ rompecabezasBarato + "<br>");
+   document.write("e- se percibio de iva " + iva + "<br>");
+
+}/*a- El tipo de juguete mas vendido
+b- El promedio de precio de los juguetes importados
+c- La recaudacion total
+d- Cual es el precio del rompecabezas nacional mas barato
+e- Cuanto se percibio de iva en total (es el 21% de las ventas) */
--- uno.js
+++ uno.js
@@ -1,3 +1,104 @@
+/*Se necesita llevar la gestión de una veterinaria. Para ello se podrán registrar los datos de cada mascota mientras el usuario quiera.
+De cada mascota se solicita:
+-nombre (entre 3 y 8 caracteres)
+-tipo (“mamifero”, “ave”, “reptil”)
+-sangre ( "fria", "calida")
+Si es de tipo reptil solo puede tener sangre fria
+-edad ( mayor a cero)
+-sexo( “m” de macho o “h” de hembra )
+Informar:
+a- Promedio de edad de los reptiles
+b- tipo y sexo de la mascota mas joven
+c- Del total de mascotas que porcentaje son aves
+d- De que tipo de mascota hay mas cantidad */
 function mostrar() {
-  alert("Ejercicio 1");
-}
+  let nombre;
+  let tipo;
+  let sangre;
+  let edad;
+  let sexo;
+  // punto A
+  let acumReptiles = 0;
+  let contadorEdadReptiles = 0;
+  let promedioEdadReptiles = 0;
+  // punto B
+  let sexoMasJoven;
+  let tipoMasJoven;
+  let edadMasJoven;
+  let flag = 1;
+  // punto C
+  let totalmascosta = 0;
+  let acumAves = 0;
+  let porcentajeAves = 0;
+  // punto D
+  let acumMamifero = 0;
+  let maxCantidad;
+  ////////////////
+  let respuesta;
+
+  do {
+    nombre = prompt("Ingrese nombre");
+		while(!(nombre.length >= 3 && nombre.length <= 10 )){
+			nombre = prompt("Nombre fuera de rango. Ingrese nombre");
+		}
+    edad = parseInt(prompt("Ingrese su edad"));
+		while (!(edad > 0)) {
+			edad =  parseInt(prompt("Error. Ingrese su edad"));
+		}
+    tipo = prompt("Ingrese un tipo de mascota (ave/reptil/mamifero)");
+    while (tipo != "mamifero" && tipo != "ave" && tipo != "reptil") {
+      tipo = prompt("Ingrese un tipo de mascota (ave/reptil/mamifero)");
+    }
+    if (tipo == "reptil") {
+      sangre = "fria"
+      acumReptiles++;
+      contadorEdadReptiles += edad;
+    }else{
+    sangre = prompt("ingrse su sangre (fria/calida)");
+    while (sangre != "fria" && sangre != "calida") {
+      sangre = prompt("ingrse su sangre (fria/calida)");
+    }}
+    sexo = prompt("Ingrese el sexo (m/h)");
+    while (sexo != "m" && sexo != "h") {
+      sexo = prompt("Error. Ingrese el sexo (m/h)");
+    }
+    if (flag || edad < edadMasJoven) {
+      edadMasJoven = edad;
+      tipoMasJoven = tipo;
+      sexoMasJoven = sexo;
+      flag = 0;
+    }
+    if (tipo == "ave") {
+      acumAves++;
+    }else if (tipo == "mamifero") {
+      acumMamifero++;
+    }
+    totalmascosta++;
+
+
+    respuesta = prompt("quiere ingresar otra mascota (si/no)");
+  } while (respuesta == "si");
+  if (acumReptiles != 0) {
+    promedioEdadReptiles = contadorEdadReptiles / acumReptiles;
+  }
+  porcentajeAves = acumAves / totalmascosta;
+  if(acumAves > acumMamifero && acumAves > acumReptiles){
+    maxCantidad = "aves";
+  }else if (acumMamifero >= acumAves && acumMamifero > acumReptiles){
+   maxCantidad = "mamifero";
+  }else {
+   maxCantidad = "reptil";
+  }
+  document.write("a- el promedio de edad de los reptiles es " + promedioEdadReptiles + "<br>");
+  document.write("b- la mas cota mas joven es de tipo " + tipoMasJoven + " su sexo " + sexoMasJoven +
+  " y su edad " + edadMasJoven + "<br>");
+  document.write("c- el porcentaje de aves es " + porcentajeAves + "<br>");
+  document.write("d- la mayor cantidad de mascota es " + maxCantidad + "<br>");
+
+
+
+  
+}/*a- Promedio de edad de los reptiles
+b- tipo y sexo de la mascota mas joven
+c- Del total de mascotas que porcentaje son aves
+d- De que tipo de mascota hay mas cantidad */
